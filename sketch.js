a009cff8c84aa4874b69284d7a09b3f54b08c3ba
Colm Rafferty <colmjr@outlook.com>
2025-11-10 21:31:30 +0800

    Fixed human drop function

--- sketch.js
+++ sketch.js
@@ -7,9 +7,6 @@ function setup() {
   rectMode(CENTER);
   colorMode(RGB,255);
   background(215, 185, 96);
-}
-
-function draw() {
   fill(0);
   for (let i = 0; i<=GRIDWIDTH; i+=30){
     line(i,0,i,GRIDHEIGHT);
@@ -18,12 +15,18 @@ function draw() {
   for (let j = 0; j<=GRIDHEIGHT; j+=30){
     line(0,j,GRIDWIDTH,j);
   }
-  drop(8,2,1)
-  drop(3,7,2)
-  drop(8,3,1)
-  drop(4,6,2)
+}
+
+function draw() {
+  if (mouseIsPressed){
+    let [x,y]= human_encoder(mouseX,mouseY);
+    drop(x,y,1);
+  }
 }
 function drop(x,y,symbol){//gomoku symbol mapping:1=white,2=black
   fill((2-symbol)*255);
   circle(x*30,y*30,25);
+}
+function human_encoder(x,y){
+  return [Math.round(x/30),Math.round(y/30)];
 }
